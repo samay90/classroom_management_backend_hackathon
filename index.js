4de2dce519aed49b5be2cbb884d1ff3b251c2edc
samay90 <samaypatel90@gmail.com>
2024-09-23 12:18:08 +0530

    Basic Strucure

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const expressFileUpload = require("express-fileupload")
+const bodyParser = require("body-parser")
+const router = require("./src/routes/route")
+
+app.use(expressFileUpload({
+    createParentPath:true
+}))
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors())
+app.use("/",router)
+app.listen(9921)

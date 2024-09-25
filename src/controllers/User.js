7c4aee53d53f8b80c3e9ed64a3dd2505915eb354
samay90 <samaypatel90@gmail.com>
2024-09-25 14:33:32 +0530

    Apply for Doctor completed!!

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,7 +1,95 @@
 const express = require("express")
 const userRouter = express.Router()
-userRouter.post("/profile/update",(req,res)=>{
-    
+const lang = require("../../lang/lang.json")
+const checker = require("../helpers/functions/checker")
+const { checkDoctorFlag, addDoctorRequest } = require("../modules/user")
+const lengthChecker = require("../helpers/functions/lengthChecker")
+const rules = require("../../rules/rules.json")
+userRouter.post("/apply/doctor",async (req,res)=>{
+    const body = req.body;
+    const user = req.user;
+    const checkerResponse = checker(body,["hospital","specialization","education","open_time","close_time"])
+    if(checkerResponse.error){
+        res.status(400).send({
+            status:400,
+            error:true,
+            message:lang.PLEASE_ENTER+checkerResponse.empty.join(", ")+"!!",
+            data:{}
+        })
+    }else{
+        if (!(typeof(body.hospital)=="string" && typeof(body.specialization)=="string" && typeof(body.education)=="string")){          
+            res.status(400).send({
+                status:400,
+                error:true,
+                message:lang.STRING_VALUES,
+                data:{}
+            })
+        }else{
+            if(!(typeof(body.open_time)=="number" && typeof(body.close_time)=="number")){
+                res.status(400).send({
+                    status:400,
+                    error:true,
+                    message:lang.NUMBER_VALIES,
+                    data:{}
+                })
+            }else{   
+                const lengthCheckerResponse = lengthChecker(body,rules)
+                if (lengthCheckerResponse.error){
+                    res.status(400).send({
+                        status:400,
+                        error:true,
+                        message:lengthCheckerResponse.message,
+                        data:{}
+                    })
+                }else{
+                    const checkDoctorFlagResponse = await checkDoctorFlag({user_id:user.user_id})
+                    if (checkDoctorFlagResponse.flag==1){
+                        res.status(400).send({
+                            status:400,
+                            error:true,
+                            message:lang.ALREADY_DOCTOR,
+                            data:{}
+                        })
+                    }else{
+                        if (!(0<=body.open_time && body.open_time<=24 && 0<=body.close_time && body.close_time<=24)){
+                            res.status(400).send({
+                                status:400,
+                                error:true,
+                                message:lang.INVALID_TIME,
+                                data:{}
+                            })
+                        }else{
+                            if (body.close_time<=body.open_time){
+                                res.status(400).send({
+                                    status:400,
+                                    error:true,
+                                    message:lang.INVALID_CLOSE_TIME,
+                                    data:{}
+                                })
+                            }else{
+                                const addDoctorRequestResponse = await addDoctorRequest({user_id:user.user_id,hospital:body.hospital,specialization:body.specialization,education:body.education,open_time:body.open_time,close_time:body.close_time})
+                                if (!addDoctorRequestResponse){
+                                    res.status(501).send({
+                                        status:501,
+                                        error:true,
+                                        message:lang.SOMETHING_WENT_WRONG,
+                                        data:{}
+                                    })
+                                }else{
+                                    res.send({
+                                        status:200,
+                                        error:false,
+                                        message:"Request Successfully Added!!",
+                                        data:{}
+                                    })
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 })
-
+userRouter
 module.exports = userRouter
--- src/modules/user.js
+++ src/modules/user.js
@@ -1,4 +1,5 @@
 const db = require("../helpers/database/db");
+const {getTimeString} = require("../helpers/functions/timeToWordDate")
 
 const verifyUser = ({user_id}) =>{
     return new Promise((resolve,reject)=>{
@@ -12,4 +13,29 @@ const verifyUser = ({user_id}) =>{
         })
     })
 }
-module.exports = {verifyUser}
+const checkDoctorFlag = ({user_id}) =>{
+    return new Promise((resolve,reject)=>{
+        const q = `select count(*) as flag from doctors where user_id=?;`;
+        db.query(q,[user_id],(err,result)=>{
+            if (err){
+                reject(err)
+            }else{
+                resolve(result[0])
+            }
+        })
+    })
+}
+const addDoctorRequest = ({user_id,hospital,specialization,education,open_time,close_time}) =>{
+    return new Promise((resolve,reject)=>{
+        const currentTime = getTimeString()
+        const q = `insert into doctors (user_id,hospital,specialization,is_verified,education,created_at,updated_at,ratings,appointment_count,open_time,close_time,appointment_status) values (?);`;
+        db.query(q,[[user_id,hospital,specialization,0,education,currentTime,currentTime,0,0,open_time,close_time,0]],(err,result)=>{
+            if (err){
+                reject(err)
+            }else{
+                resolve(result)
+            }
+        })
+    })
+}
+module.exports = {verifyUser,checkDoctorFlag,addDoctorRequest}
